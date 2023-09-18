dea879a734c113eea9999f879a049f2ee5896eeb
remersiveross <87240474+remersiveross@users.noreply.github.com>
2023-09-18 15:02:01 -0400

    Update moovAccountCreate.js

--- moovAccountCreate.js
+++ moovAccountCreate.js
@@ -1,18 +1,19 @@
 const axios = require('axios');
 
+// Class for creating a Moov account
 class CreateAccount {
   constructor(userProfile) {
-    this.moovClient = new Client();
     this.userProfile = userProfile;
+    this.moovClient = new Client();
     this.logger = Log.channel('moov');
   }
 
-  // Checks if the account type is individual
+  // Check if the account is for an individual
   isIndividual() {
     return this.userProfile.entity_profile_type_id == 1;
   }
 
-  // Executes the account creation process based on the account type
+  // Execute the account creation process
   execute() {
     if (this.isIndividual()) {
       return this.createIndividualAccount();
@@ -21,7 +22,7 @@ class CreateAccount {
     return this.createBusinessAccount();
   }
 
-  // Creates an individual account by making a request to the Moov API
+  // Create an individual account
   createIndividualAccount() {
     const response = this.moovClient.createAccount(
       this.prepareIndividualAccountRequest()
@@ -33,7 +34,7 @@ class CreateAccount {
     return cleanedResponse;
   }
 
-  // Requests capabilities for the Moov account
+  // Request capabilities for the account
   requestCapabilities() {
     const scopes = [
       `/accounts/${this.userProfile.moov_account_id}/profile.read`,
@@ -42,6 +43,7 @@ class CreateAccount {
       `/accounts/${this.userProfile.moov_account_id}/capabilities.write`,
     ];
     this.moovClient.refreshAccessToken(scopes.join(','));
+
     const response = this.moovClient.requestCapabilities(
       this.userProfile.moov_account_id,
       {
@@ -53,215 +55,183 @@ class CreateAccount {
     this.logger.debug('RequestCapabilities response', cleanedResponse);
   }
 
-  // Cleans the response from the Moov API
+  // Clean the response by parsing it as JSON
   cleanResponse(response) {
-    return JSON.parse(response.getBody().getContents());
+    return JSON.parse(response.body);
   }
 
-  // Prepares the request for creating an individual account
+  // Prepare the request for creating an individual account
   prepareIndividualAccountRequest() {
-    const user = this.userProfile.user;
-    const country = this.userProfile.country;
     const request = {
       accountType: 'individual',
       profile: {
         individual: {
-          name: {
-            firstName: this.userProfile.first_name,
-            middleName: this.userProfile.middle_name,
-            lastName: this.userProfile.last_name,
-          },
+          firstName: this.userProfile.first_name,
+          lastName: this.userProfile.last_name,
           phone: {
             number: this.userProfile.phone,
-            countryCode: country.phone_code,
+            countryCode: '1',
           },
-          email: user.email,
+          email: this.userProfile.user.email,
           address: {
             addressLine1: this.userProfile.address1,
             addressLine2: this.userProfile.address2,
             city: this.userProfile.city,
-            stateOrProvince: this.userProfile.state.name,
+            stateOrProvince: this.userProfile.state.state_code,
             postalCode: this.userProfile.zipcode,
-            country: country.iso2,
+            country: 'US',
           },
           birthDate: {
-            day: parseInt(this.userProfile.date_of_birth.format('d')),
-            month: parseInt(this.userProfile.date_of_birth.format('M')),
-            year: parseInt(this.userProfile.date_of_birth.format('YYYY')),
+            day: this.userProfile.date_of_birth.getDate(),
+            month: this.userProfile.date_of_birth.getMonth() + 1,
+            year: this.userProfile.date_of_birth.getFullYear(),
+          },
+          governmentID: {
+            ssn: {
+              full: this.userProfile.tax_id_cr.replace(/[- ]/g, ''),
+            },
           },
         },
       },
       termsOfService: {
-        token: getTosToken(),
+        token: this.getTosToken(),
       },
     };
 
+    this.logger.debug('Moov Individual account request', request);
     return request;
   }
 
-  // Creates a business account by making a request to the Moov API
-  createBusinessAccount = async () => {
-    try {
-      const user = userProfile.user;
-      const description = userProfile.entity_tax_id_cr ? 'Investment Entity' : 'Family Trust';
-      const country = userProfile.country;
-      const entityAddress = userProfile.entityLegalAddress();
-  
-      const request = {
-        accountType: 'business',
-        profile: {
-          business: {
-            legalBusinessName: userProfile.entity_name,
-            businessType: entityType.getMoovBusinessType(),
-            phone: {
-              number: userProfile.phone,
-              countryCode: '1',
-            },
-            email: user.email,
-            description: description,
-            address: {
-              addressLine1: entityAddress.address1,
-              addressLine2: entityAddress.address2,
-              city: entityAddress.city,
-              stateOrProvince: entityAddress.state.name,
-              postalCode: entityAddress.zipcode,
-              country: country.iso2,
-            },
-            industryCodes: {
-              mcc: '8999',
-            },
-            taxID: {
-              ein: {
-                number: userProfile.entity_tax_id_cr.replace(' ', ''),
-              },
+  // Get the terms of service token
+  getTosToken() {
+    const tosResponse = this.moovClient.getTermsOfServiceToken();
+    return JSON.parse(tosResponse.body).token;
+  }
+
+  // Create a business account
+  createBusinessAccount() {
+    const response = this.moovClient.createAccount(
+      this.prepareBusinessAccountRequest()
+    );
+    const cleanedResponse = this.cleanResponse(response);
+
+    this.logger.debug('Moov response', cleanedResponse);
+
+    this.addBeneficialOwner(cleanedResponse.accountID);
+
+    return cleanedResponse;
+  }
+
+  // Prepare the request for creating a business account
+  prepareBusinessAccountRequest() {
+    if (!this.userProfile.entityType) {
+      throw new Error('Entity type not found while creating business account');
+    }
+
+    const description = this.userProfile.entity_tax_id_cr
+      ? 'Investment Entity'
+      : 'Family Trust';
+    const entityAddress = this.userProfile.entityLegalAddress();
+    const request = {
+      accountType: 'business',
+      profile: {
+        business: {
+          legalBusinessName: this.userProfile.entity_name,
+          businessType: this.userProfile.entityType.getMoovBusinessType(),
+          phone: {
+            number: this.userProfile.phone,
+            countryCode: '1',
+          },
+          email: this.userProfile.user.email,
+          description: description,
+          address: {
+            addressLine1: entityAddress.address1,
+            addressLine2: entityAddress.address2,
+            city: entityAddress.city,
+            stateOrProvince: entityAddress.state.name,
+            postalCode: entityAddress.zipcode,
+            country: entityAddress.country.iso2,
+          },
+          industryCodes: {
+            mcc: '8999',
+          },
+          taxID: {
+            ein: {
+              number: this.userProfile.entity_tax_id_cr.replace(/ /g, ''),
             },
           },
         },
-        termsOfService: {
-          token: getTosToken(),
-        },
-      };
-  
-      console.debug('Moov Business account request', request);
-      return request;
-    } catch (error) {
-      console.error(error);
-      throw new Error('Failed to create business account');
-    }
-  };
+      },
+      termsOfService: {
+        token: this.getTosToken(),
+      },
+    };
 
-  // Adds a beneficial owner to the Moov account
-  addBeneficialOwner = async (moovAccountId) => {
-    try {
-      const user = userProfile.user;
-      const scopes = [
-        `/accounts/${moovAccountId}/profile.read`,
-        `/accounts/${moovAccountId}/profile.write`,
-        `/accounts/${moovAccountId}/representatives.read`,
-        `/accounts/${moovAccountId}/representatives.write`,
-      ];
-      moovClient.refreshAccessToken(scopes.join(','));
-  
-      const response = await moovClient.addRepresentative(moovAccountId, {
-        name: {
-          firstName: userProfile.first_name,
-          middleName: userProfile.middle_name,
-          lastName: userProfile.last_name,
-        },
-        phone: {
-          number: userProfile.phone,
-          countryCode: '1',
-        },
-        email: user.email,
-        address: {
-            month:
-            const createBusinessAccount = async () => {
-                try {
-                  const user = userProfile.user;
-                  const description = userProfile.entity_tax_id_cr ? 'Investment Entity' : 'Family Trust';
-                  const country = userProfile.country;
-                  const entityAddress = userProfile.entityLegalAddress();
-              
-                  const request = {
-                    accountType: 'business',
-                    profile: {
-                      business: {
-                        legalBusinessName: userProfile.entity_name,
-                        businessType: entityType.getMoovBusinessType(),
-                        phone: {
-                          number: userProfile.phone,
-                          countryCode: '1',
-                        },
-                        email: user.email,
-                        description: description,
-                        address: {
-                          addressLine1: entityAddress.address1,
-                          addressLine2: entityAddress.address2,
-                          city: entityAddress.city,
-                          stateOrProvince: entityAddress.state.name,
-                          postalCode: entityAddress.zipcode,
-                          country: country.iso2,
-                        },
-                        industryCodes: {
-                          mcc: '8999',
-                        },
-                        taxID: {
-                          ein: {
-                            number: userProfile.entity_tax_id_cr.replace(' ', ''),
-                          },
-                        },
-                      },
-                    },
-                    termsOfService: {
-                      token: getTosToken(),
-                    },
-                  };
-              
-                  console.debug('Moov Business account request', request);
-                  return request;
-                } catch (error) {
-                  console.error(error);
-                  throw new Error('Failed to create business account');
-                }
-              };
-              
-              const addBeneficialOwner = async (moovAccountId) => {
-                try {
-                  const user = userProfile.user;
-                  const scopes = [
-                    `/accounts/${moovAccountId}/profile.read`,
-                    `/accounts/${moovAccountId}/profile.write`,
-                    `/accounts/${moovAccountId}/representatives.read`,
-                    `/accounts/${moovAccountId}/representatives.write`,
-                  ];
-                  moovClient.refreshAccessToken(scopes.join(','));
-              
-                  const response = await moovClient.addRepresentative(moovAccountId, {
-                    name: {
-                      firstName: userProfile.first_name,
-                      middleName: userProfile.middle_name,
-                      lastName: userProfile.last_name,
-                    },
-                    phone: {
-                      number: userProfile.phone,
-                      countryCode: '1',
-                    },
-                    email: user.email,
-                    address: {
-                      addressLine1: userProfile.address1,
-                      addressLine2: userProfile.address2,
-                      city: userProfile.city,
-                      stateOrProvince: userProfile.state.state_code,
-                      postalCode: userProfile.zipcode,
-                      country: 'US',
-                    },
-                    birthDate: {
-                      day: userProfile.date_of_birth.getDate(),
-                      month: userProfile.date_of_birth.getMonth() + 1,
-                      year: userProfile.date_of_birth.getFullYear(),
-                    },
-                    governmentID: {
-                      ssn: {
-                        full: userProfile.tax_id_cr.replace(' ', ''),
-                        lastFour: userProfile.tax_id_cr.trim().slice(-4),
-                      },
+    this.logger.debug('Moov Business account request', request);
+    return request;
+  }
+
+addBeneficialOwner(moovAccountId) {
+  const scopes = [
+    `/accounts/${moovAccountId}/profile.read`,
+    `/accounts/${moovAccountId}/profile.write`,
+    `/accounts/${moovAccountId}/representatives.read`,
+    `/accounts/${moovAccountId}/representatives.write`,
+  ];
+  this.moovClient.refreshAccessToken(scopes.join(','));
+
+  const response = this.moovClient.addRepresentative(moovAccountId, {
+    name: {
+      firstName: this.userProfile.first_name,
+      middleName: this.userProfile.middle_name,
+      lastName: this.userProfile.last_name,
+    },
+    phone: {
+      number: this.userProfile.phone,
+      countryCode: '1',
+    },
+    email: this.userProfile.user.email,
+    address: {
+      addressLine1: this.userProfile.address1,
+      addressLine2: this.userProfile.address2,
+      city: this.userProfile.city,
+      stateOrProvince: this.userProfile.state.state_code,
+      postalCode: this.userProfile.zipcode,
+      country: 'US',
+    },
+    birthDate: {
+      day: this.userProfile.date_of_birth.getDate(),
+      month: this.userProfile.date_of_birth.getMonth() + 1,
+      year: this.userProfile.date_of_birth.getFullYear(),
+    },
+    governmentID: {
+      ssn: {
+        full: this.userProfile.tax_id_cr.replace(/ /g, ''),
+        lastFour: this.userProfile.tax_id_cr.trim().slice(-4),
+      },
+    },
+    responsibilities: {
+      isController: true,
+      isOwner: true,
+      ownershipPercentage: 100,
+      jobTitle: this.userProfile.title || 'Owner',
+    },
+  });
+  this.logger.debug(
+    'Moov addBeneficialOwner response',
+    this.cleanResponse(response)
+  );
+
+  const responsePatch = this.moovClient.patchAccount(moovAccountId, {
+    profile: {
+      business: {
+        ownersProvided: true,
+      },
+    },
+  });
+  this.logger.debug(
+    'Moov patchAccount response',
+    this.cleanResponse(responsePatch)
+  );
+}
+}
